c638882e269b267348b1bb2d8b1bfbae41f50909
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2025-07-10 23:45:42 -0300

    start spring project

---
+++ src/main/kotlin/com/vanderloureiro/applink_api/ApplinkApiApplication.kt
@@ -0,0 +1,11 @@
+package com.vanderloureiro.applink_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApplinkApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApplinkApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/vanderloureiro/applink_api/ApplinkApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vanderloureiro.applink_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplinkApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
