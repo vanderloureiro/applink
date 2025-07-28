68fae654dec41f00fcfb11ab816b3a46b982d635
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2025-07-28 20:57:02 -0300

    add api prefix

--- src/main/kotlin/com/vanderloureiro/applink_api/link/LinkController.kt
+++ src/main/kotlin/com/vanderloureiro/applink_api/link/LinkController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/links")
+@RequestMapping("/api/links")
 @CrossOrigin(origins = ["http://localhost:3000"])
 class LinkController(val linkRepository: LinkRepository) {
 
