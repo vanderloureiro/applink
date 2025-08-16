a88d6769d3bc274518f31d8e2a80cc8aa6a311c6
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2025-08-15 22:24:44 -0300

    create main css

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: [
+    '~/assets/css/main.css'
+  ],
   modules: ['@nuxt/eslint', '@nuxt/icon', '@pinia/nuxt'],
   runtimeConfig: {
     public: {
