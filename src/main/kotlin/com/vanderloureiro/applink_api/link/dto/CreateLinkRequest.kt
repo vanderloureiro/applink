a4bff68a39691b4a79f93fd73a959f8ab4134b9b
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2025-09-06 17:12:28 -0300

    ignore ownerId temporarily

--- src/main/kotlin/com/vanderloureiro/applink_api/link/dto/CreateLinkRequest.kt
+++ src/main/kotlin/com/vanderloureiro/applink_api/link/dto/CreateLinkRequest.kt
@@ -7,11 +7,10 @@ import java.util.UUID
 data class CreateLinkRequest(
     val title: String,
     val url: String,
-    val description: String?,
-    val ownerId: UUID
+    val description: String?
 ) {
 
     fun toDomain(): Link {
-        return Link(title = this.title, path = this.url, description = this.description, owner = User(id = ownerId))
+        return Link(title = this.title, path = this.url, description = this.description, owner = User(id = UUID.fromString("9dbdb559-55b6-4d64-88b7-7c93c88190bd")))
     }
 }
