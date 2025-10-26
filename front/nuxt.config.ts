d3f6cd2651d2dc2476dac5abe1d2eb5e898eeed4
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2025-10-26 15:09:31 -0300

    change nuxt baseUrl

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/icon', '@pinia/nuxt'],
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8080'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || ''
     }
   },
   app: {
