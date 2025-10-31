47ab7361c310cc9885f79d864317fb1081c7e5a8
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2025-10-31 17:54:16 -0300

    remove build logging

--- src/main/kotlin/com/vanderloureiro/applink_api/user/UserController.kt
+++ src/main/kotlin/com/vanderloureiro/applink_api/user/UserController.kt
@@ -8,12 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import io.swagger.v3.oas.annotations.tags.Tags
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 @Tags(Tag(name = "User", description = "User resources"))
 class UserController(
     private val userService: UserService,
-    private val authService: AuthService,
+    private val authService: AuthService
 ) {
     @PostMapping
     @ApiResponse(description = "Create an user")
