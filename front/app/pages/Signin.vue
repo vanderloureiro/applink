a2f965449e9926dd51c9269ea6efeee64e8e5b3a
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2025-11-22 17:51:11 -0300

    maintennance mode in integration

--- front/app/pages/Signin.vue
+++ front/app/pages/Signin.vue
@@ -22,6 +22,7 @@
               @keyup.enter="sendEmail"
             />
           </div>
+          
           <div class="field accept-terms" v-if="isOnboardingUser">
             <label>
               <input type="checkbox" name="acceptTerms" v-model="acceptTerms" />
